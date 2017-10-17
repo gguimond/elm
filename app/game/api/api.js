c44f34df794003a735b2f2f1cf182451cd273861
Guillaume Guimond <guimog@gmail.com>
2017-10-17 09:16:46 +0200

    getting resources

--- app/game/api/api.js
+++ app/game/api/api.js
@@ -6,7 +6,7 @@ var server = jsonServer.create()
 // Définit les intergiciels (*middlewares*) par défaut (logger, static, cors et no-cache)
 server.use(jsonServer.defaults())
 
-var router = jsonServer.router('db.json')
+var router = jsonServer.router('./api/db.json')
 server.use(router)
 
 console.log('Listening at 4000')
